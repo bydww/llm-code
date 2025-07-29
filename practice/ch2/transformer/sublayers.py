7ca0972c4ace03c790760bea96aa5fdc40f2d6d7
wl <3581468327@qq.com>
2025-07-29 21:18:30 +0800

    20250729

--- practice/ch2/transformer/sublayers.py
+++ practice/ch2/transformer/sublayers.py
@@ -1,9 +1,25 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F # type: ignore
+import torch.nn.functional as F
 import math
 
 
 '''多头注意力机制'''
-
-class MultiHeadAttention(nn.)
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model,heads,dropout=0.1):
+        super().__init__()
+        self.d_model = d_model
+        self.d_k = d_model // heads
+        self.h = heads
+        
+        self.q_linear = nn.Linear(d_model,d_model)
+        self.k_linear = nn.Linear(d_model,d_model)
+        self.v_linear = nn.Linear(d_model,d_model)
+        
+        self.out = nn.Linear(d_model,d_model)
+        self.dropout = nn.Dropout(dropout)
+    
+    def attention(self,q,k,v,d_k,mask=None,dropout=None):
+        scores = torch.matmul
+        
+        
